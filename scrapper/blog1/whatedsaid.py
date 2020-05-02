26fc958ba116b061f42e4af7415902f40312e0f9
oanadinca <47577151+oanadinca@users.noreply.github.com>
2020-05-02 20:39:36 +0300

    code refactoring into functions

--- scrapper/blog1/whatedsaid.py
+++ scrapper/blog1/whatedsaid.py
@@ -16,7 +16,6 @@ def get_last_post_id_from_file(file_name):
     return -1
 
 
-# TODO: how to append per post all the comments?
 def append_posts_to_file(file_name, posts):
     for post in posts:
         if len(post.comments) == 0:
@@ -31,6 +30,7 @@ def append_posts_to_file(file_name, posts):
                                                comment.msg.encode('utf-8')])
 
 
+# TODO: how to append per post all the comments?
 def append_post_to_file(file_name, post):
     if len(post.comments) == 0:
         append_list_as_row(file_name, [post.id, post.headline.encode('utf-8'), post.summary.encode('utf-8'),
@@ -45,11 +45,8 @@ def append_post_to_file(file_name, post):
 
 
 def append_list_as_row(file_name, list_of_elem):
-    # Open file in append mode
     with open(file_name, 'a+', newline='') as write_obj:
-        # Create a writer object from csv module
         csv_writer = csv.writer(write_obj)
-        # Add contents of list as last row in the csv file
         csv_writer.writerow(list_of_elem)
 
 
@@ -59,9 +56,70 @@ def first_open_csv(file_name, fieldnames):
         csv_writer.writerow(fieldnames)
 
 
-def main():
+def get_soup(url):
+    try:
+        source = requests.get(url).text
+        soup = BeautifulSoup(source, 'lxml')
+        return soup
+    except Exception as e:
+        return e
+
+
+def get_headline(article):
+    return article.header.h1.a.text
+
+
+def get_summary(summary_soup):
+    summary_list = summary_soup.find('div', class_='entry-content').find_all('p')
+    post_summary = ""
+    for s in summary_list:
+        post_summary = post_summary + " " + s.text.replace('\n', '').replace('\t', '') \
+            # .replace('\U0001f642', ':)').replace('मुलांचे हक्क', '')
+    # print(post_summary)
+    return post_summary
+
+
+def get_username(comment):
+    try:
+        username = comment.find('div', class_='comment-author vcard').b.a.text.replace('\n', '').replace('\t', '')
+    except Exception as e:
+        try:
+            username = comment.find('div', class_='comment-author vcard').b.text.replace('\n', '').replace('\t', '')
+        except Exception as e:
+            username = ''
+    return username
+
+
+def get_timestamp(comment):
+    try:
+        date = comment.find('div', class_='comment-metadata').a.time.text.replace('\n', '').replace('\t', '')
+        parsed_date = parse(date)
+        timestamp = datetime.datetime.timestamp(parsed_date)
+    except Exception as e:
+        timestamp = ''
+    return timestamp
+
+
+def get_message(comment):
+    msg = ""
+    try:
+        msg_list = comment.find('div', class_='comment-content').find_all('p')
+        like_buttons = comment.find('div', class_='comment-content').find('p',
+                                                                          class_='comment-likes comment-not-liked')
+        if like_buttons in msg_list:
+            msg_list.remove(like_buttons)
+        for m in msg_list:
+            msg = msg + m.text.replace('\n', '').replace('\t', '') \
+                # .replace('\U0001f642', ':)')
+    except Exception as e:
+        msg = ''
+    return msg
+
+
+def main(args):
     post_id = 0
     posts = []
+
     file_name = 'blog1/whatedsaid_scrape.csv'
     fieldnames = ['post_id', "post_headline", "post_summary", "post_url", "comment_id", "comment_ref_id",
                   "comment_timestamp", "comment_username", "comment_replied_to", "comment_message"]
@@ -70,86 +128,52 @@ def main():
     except Exception as e:
         first_open_csv(file_name, fieldnames)
 
-    source = requests.get('https://whatedsaid.wordpress.com').text
-    soup = BeautifulSoup(source, 'lxml')
+    page_soup = get_soup(args.url)
+    if isinstance(page_soup, Exception):
+        return
 
-    archive_list = soup.find('select', id='archives-dropdown-5').find_all('option')
-    archive_list.pop(0)  # select month phrase with no link to use
+    archive_list = page_soup.find('select', id='archives-dropdown-5').find_all('option')
+    archive_list.pop(0)  # select month phrase with no url to use
 
-    for i in range(0, 2):
+    for i in range(2, 3):
         selection = archive_list[i]
-        page_link = selection['value']
-        # print(page_link)
-        article_source = requests.get(page_link).text
-        article_soup = BeautifulSoup(article_source, 'lxml')
+        page_url = selection['value']
+        # print(page_url)
+        article_soup = get_soup(page_url)
+        if isinstance(article_soup, Exception):
+            break
 
         articles = article_soup.find_all('article')
         for article in articles:
             post_id = post_id + 1
-            post_headline = article.header.h1.a.text
+            post_headline = get_headline(article)
             post_url = article.header.h1.a['href']
-
-            summary_source = requests.get(post_url).text
-            # print(headline)
-            summary_soup = BeautifulSoup(summary_source, 'lxml')
-            summary_list = summary_soup.find('div', class_='entry-content').find_all('p')
-            post_summary = ""
-            for s in summary_list:
-                post_summary = post_summary + " " + s.text.replace('\n', '').replace('\t', '')\
-                    # .replace('\U0001f642', ':)').replace('मुलांचे हक्क', '')
-            # print(post_summary)
+            summary_soup = get_soup(post_url)
+            if isinstance(summary_soup, Exception):
+                return
+            post_summary = get_summary(summary_soup)
             try:
                 comments = summary_soup.find('ol', class_='comment-list').find_all('li')
             except Exception as e:
                 comments = []
 
             comment_id = 0
-            comm_list = []
+            comments_final_list = []
             anonymous = 1
 
             for comment in comments:
                 comment_id = comment_id + 1
                 comment_ref_id = 0
-                try:
-                    user = comment.find('div', class_='comment-author vcard').b.a.text.replace('\n', '').replace(
-                        '\t',
-                        '')
-                except Exception as e:
-                    try:
-                        user = comment.find('div', class_='comment-author vcard').b.text.replace('\n', '').replace(
-                            '\t',
-                            '')
-                    except Exception as e:
-                        user = ''
-
-                if user == 'Unknown' or user == 'Anonymous':
-                    user = 'Anonymous_user_' + str(anonymous)
+                username = get_username(comment)
+                if username == 'Unknown' or username == 'Anonymous':
+                    username = 'Anonymous_user_' + str(anonymous)
                     anonymous = anonymous + 1
+                timestamp = get_timestamp(comment)
+                msg = get_message(comment)
 
-                try:
-                    date = comment.find('div', class_='comment-metadata').a.time.text.replace('\n', '').replace(
-                        '\t', '')
-                    parsed_date = parse(date)
-                    timestamp = datetime.datetime.timestamp(parsed_date)
-                except Exception as e:
-                    timestamp = ''
-
-                msg = ""
-                try:
-                    msg_list = comment.find('div', class_='comment-content').find_all('p')
-                    like_buttons = comment.find('div', class_='comment-content').find('p',
-                                                                                      class_='comment-likes comment-not-liked')
-                    if like_buttons in msg_list:
-                        msg_list.remove(like_buttons)
-                    for m in msg_list:
-                        msg = msg + m.text.replace('\n', '').replace('\t', '')\
-                            # .replace('\U0001f642', ':)')
-                except Exception as e:
-                    msg = ''
-
-                if not (user == '' and msg == '' and timestamp == ''):
-                    comm = Comment(comment_id, comment_ref_id, timestamp, user, "", msg)
-                    comm_list.append(comm)
+                if not (username == '' and msg == '' and timestamp == ''):
+                    comm = Comment(comment_id, comment_ref_id, timestamp, username, "", msg)
+                    comments_final_list.append(comm)
                 else:
                     comment_id = comment_id - 1
 
@@ -158,54 +182,21 @@ def main():
                     try:
                         replies = comment.find('ol', class_='children').find_all('li')
                         comment_ref_id = comment_id
-                        replied_to = user
-                        print(user)
+                        replied_to = username
+                        # print(user)
 
                         for reply in replies:
-                            print('ok')
                             comment_id = comment_id + 1
-                            try:
-                                user = reply.find('div', class_='comment-author vcard').b.a.text.replace('\n',
-                                                                                                         '').replace(
-                                    '\t', '')
-                            except Exception as e:
-                                try:
-                                    if user == '':
-                                        user = reply.find('div', class_='comment-author vcard').b.text.replace('\n',
-                                                                                                               '').replace(
-                                            '\t', '')
-                                except Exception as e:
-                                    user = ''
-
-                            if user == 'Unknown' or user == 'Anonymous':
-                                user = 'Anonymous_user_' + str(anonymous)
+                            username = get_username(reply)
+                            if username == 'Unknown' or username == 'Anonymous':
+                                username = 'Anonymous_user_' + str(anonymous)
                                 anonymous = anonymous + 1
+                            timestamp = get_timestamp(reply)
+                            msg = get_message(reply)
 
-                            try:
-                                date = reply.find('div', class_='comment-metadata').a.time.text.replace('\n',
-                                                                                                        '').replace(
-                                    '\t', '')
-                                parsed_date = parse(date)
-                                timestamp = datetime.datetime.timestamp(parsed_date)
-                            except Exception as e:
-                                timestamp = ''
-
-                            msg = ""
-                            try:
-                                msg_list = reply.find('div', class_='comment-content').find_all('p')
-                                like_buttons = comment.find('div', class_='comment-content').find('p',
-                                                                                                  class_='comment-likes comment-not-liked')
-                                if like_buttons in msg_list:
-                                    msg_list.remove(like_buttons)
-                                for m in msg_list:
-                                    msg = msg + m.text.replace('\n', '').replace('\t', '')\
-                                        # .replace('\U0001f642', ':)')  # smiley face
-                            except Exception as e:
-                                msg = ''
-
-                            if not (user == '' and msg == '' and timestamp == ''):
-                                comm = Comment(comment_id, comment_ref_id, timestamp, user, replied_to, msg)
-                                comm_list.append(comm)
+                            if not (username == '' and msg == '' and timestamp == ''):
+                                comm = Comment(comment_id, comment_ref_id, timestamp, username, replied_to, msg)
+                                comments_final_list.append(comm)
                             else:
                                 comment_id = comment_id - 1
 
@@ -213,14 +204,12 @@ def main():
                     except Exception as e:
                         fail_condition = False
 
-            posts.append(Post(post_id, post_headline, post_summary, post_url, comm_list))
+            posts.append(Post(post_id, post_headline, post_summary, post_url, comments_final_list))
 
-    # append_posts_to_file(file_name, posts)
+    append_posts_to_file(file_name, posts)
     # print(post.id, post.comments)
 
-    index = 0  # trebuie modificat cu primul index din range-ul de parcurgere
     for post in posts:
-        post_file_name = "blog1/post_" + str(index)
+        post_file_name = "blog1/post_" + str(post.id)
         append_post_to_file(post_file_name, post)
-        index = index + 1
 
--- scrapper/main.py
+++ scrapper/main.py
@@ -1,8 +1,14 @@
 from blog1 import whatedsaid
+from argparse import ArgumentParser
 
 
 def main():
-    whatedsaid.main()
+    parser = ArgumentParser()
+    parser.add_argument("--url", type=str,
+                        help="Please provide a blog url for data crawl!")
+    args = parser.parse_args()
+    if "whatedsaid" in args.url:
+        whatedsaid.main(args)
 
 
 if __name__ == "__main__":
